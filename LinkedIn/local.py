ceba3f9e6f398b9eb7550c28f92cb4f7427276ed
ravikiran4 <prashanth.ganoji@gmail.com>
2018-07-25 12:13:24 +0530

    final commit

---
+++ LinkedIn/local.py
@@ -0,0 +1,13 @@
+from .base import *
+
+ENVIRONMENT = 'local'
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'linkedin',
+        'HOST': 'localhost',
+        'USER':"root",
+        'PASSWORD': "prashanth"
+    }
+}
