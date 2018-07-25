80acad34fecfda4c5992e35e225e60838d7347c0
ravikiran4 <prashanth.ganoji@gmail.com>
2018-07-25 09:52:56 +0530

    fixed static files 2

--- LinkedIn/settings.py
+++ LinkedIn/settings.py
@@ -14,6 +14,7 @@ import os
 import datetime
 import dj_database_url
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -157,3 +158,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
