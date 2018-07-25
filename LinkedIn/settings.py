2353d5e5fac85285b1becf5b60bfb8bc2fd20d7c
ravikiran4 <prashanth.ganoji@gmail.com>
2018-07-25 10:37:03 +0530

    fixed static files 7

--- LinkedIn/settings.py
+++ LinkedIn/settings.py
@@ -159,5 +159,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-
-django_heroku.settings(locals())
