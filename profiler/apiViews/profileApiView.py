1036a93830eb5545b581574f156c70d6ac49354f
ravikiran4 <prashanth.ganoji@gmail.com>
2018-07-28 11:49:45 +0530

    added pagination to profile list

--- profiler/apiViews/profileApiView.py
+++ profiler/apiViews/profileApiView.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
